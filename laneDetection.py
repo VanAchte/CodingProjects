70dd92740e838c79e988b9d229abacf68a7668fd
VanAchte <cascadestriders@hotmail.com>
2017-07-11 10:49:46 -0700

    Add files via upload

--- laneDetection.py
+++ laneDetection.py
@@ -6,6 +6,22 @@ import argparse
 import matplotlib.image as mpimg
 from matplotlib import pyplot as plt
 
+# image is expected be in RGB color space
+def select_rgb_white_yellow(image):
+    # white color mask
+    lower = np.uint8([200, 200, 200])
+    upper = np.uint8([255, 255, 255])
+    white_mask = cv2.inRange(image, lower, upper)
+    # yellow color mask
+    lower = np.uint8([190, 190,   0])
+    upper = np.uint8([255, 255, 255])
+    yellow_mask = cv2.inRange(image, lower, upper)
+    # combine the mask
+    mask = cv2.bitwise_or(white_mask, yellow_mask)
+    masked = cv2.bitwise_and(image, image, mask = mask)
+    return masked
+
+
 # What if ROI doesnt contain any lane?
 # we can assume that lane is at the rightmost
 ap = argparse.ArgumentParser()
@@ -17,21 +33,23 @@ im = plt.imread(args["image"])
 testImg = cv2.imread(args["image"])
 imgEdges = cv2.Canny(testImg,100, 200)
 
-img = cv2.imread('lane.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+whiteYellowImg = select_rgb_white_yellow(cv2.imread(args["image"]))
+
+gray = cv2.cvtColor(testImg,cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,215,315,apertureSize = 3)
-cv2.imshow('edges',edges)
+#cv2.imshow('edges',edges)
 minLineLength = 5
 maxLineGap = 1
 lines = cv2.HoughLinesP(edges,1,np.pi/180,15,minLineLength,maxLineGap)
 for x in range(0, len(lines)):
     for x1,y1,x2,y2 in lines[x]:
-        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
-#cv2.imshow('lines',lines)
-cv2.imshow('hough',img)
+        cv2.line(testImg,(x1,y1),(x2,y2),(0,255,0),2)
+cv2.imshow('White Yellow Image', whiteYellowImg)
+cv2.imshow('edges', edges)
+cv2.imshow('hough',testImg)
 cv2.waitKey(0)
 
-coord = [(0, 0), (450, 0), (0, 300), (450, 300)]
+#coord = [(0, 0), (450, 0), (0, 300), (450, 300)]
 
 
 #lines = cv2.HoughLines(imgEdges, 1, np.pi / 180.0, 50, np.array([]), 100, 10)
@@ -44,7 +62,7 @@ coord = [(0, 0), (450, 0), (0, 300), (450, 300)]
 
 # coord = []
 # coord = [(241, 316), (438, 312), (602, 447), (54, 447)] DSCN0632
-#coord = [(251, 314), (443, 306), (616, 435), (85, 445)]
+coord = [(251, 314), (443, 306), (616, 435), (85, 445)]
 
 # def onclick(event):
 # 	if event.xdata != None and event.ydata != None :
@@ -60,25 +78,25 @@ coord = [(0, 0), (450, 0), (0, 300), (450, 300)]
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-cap = cv2.VideoCapture('testvideos/calibration/DSCN0623.MOV')
+# cap = cv2.VideoCapture('testvideos/calibration/DSCN0612.MOV')
 #
 # while (cap.isOpened()):
 #     ret, frame = cap.read()
 #     if frame is None:
 #         break
 #     # roi = frame[250:480, 0:640]
-#     cv2.circle(frame, (251, 314), 5, (0, 0, 255), -1)
-#     cv2.circle(frame, (443, 306), 5, (0, 0, 255), -1)
-#     cv2.circle(frame, (616, 435), 5, (0, 0, 255), -1)
-#     cv2.circle(frame, (85, 445), 5, (0, 0, 255), -1)
-#     cv2.imshow("Input", frame);
+#     # cv2.circle(frame, (251, 314), 5, (0, 0, 255), -1)
+#     # cv2.circle(frame, (443, 306), 5, (0, 0, 255), -1)
+#     # cv2.circle(frame, (616, 435), 5, (0, 0, 255), -1)
+#     # cv2.circle(frame, (85, 445), 5, (0, 0, 255), -1)
+#     cv2.imshow("Original", frame)
 #     warped = four_point_transform(frame, np.array(coord))
-#     gray = cv2.cvtColor(warped, cv2.COLOR_BGR2GRAY)
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 #
 #     # houghline transform
 #     high_thresh, thresh_im = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 #     lowThresh = 0.5 * high_thresh
-#     edges = cv2.Canny(gray, 50, 150)
+#     edges = cv2.Canny(gray, 300, 400)
 #     kernel = np.ones((3, 3), np.uint8)
 #     dilation = cv2.dilate(edges, kernel, iterations=1)
 #     im = cv2.bilateralFilter(dilation, 5, 17, 17)
@@ -98,11 +116,12 @@ cap = cv2.VideoCapture('testvideos/calibration/DSCN0623.MOV')
 #                     # print("yes")
 #                     min_x = x2
 #
-#                 cv2.line(warped, (x1, y1), (x2, y2), (0, 255, 0), 2)
+#                 cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
 #         #print((min_x - 247.0) * (60.0 / 523.0))
 #     # font = cv2.FONT_HERSHEY_SIMPLEX
 #     # cv2.putText(frame,'Hello World!',(10,500), font, 1,(255,255,255),2)
-#     cv2.imshow('frame', warped)
+#     cv2.imshow('edges', edges)
+#     cv2.imshow('frame', frame)
 #     # cv2.waitKey(0)
 #     # cv2.waitKey(0)
 #     if cv2.waitKey(1) & 0xFF == ord('q'):
