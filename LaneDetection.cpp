9e9cae05bfb72d4df1d3a058f7093e342b4bfc97
VanAchte <cascadestriders@hotmail.com>
2017-09-19 16:28:20 -0700

    Latest Line following algorithm

--- LaneDetection.cpp
+++ LaneDetection.cpp
@@ -1,7 +1,15 @@
+/*
+* Author: Conor J Van Achte
+* Last Edited: 09/13/2017
+*
+*
+*/
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 using namespace cv;
@@ -29,7 +37,7 @@ Mat yellowFilter(const Mat& src) {
 
 vector<Point> initFitLine(const vector<vector<Point>>& points, int index) {
 	//cout << "Start of initFitLine" << endl;
-	waitKey(200);
+	//waitKey(200);
 	int size = points[index].size();
 	vector<Point> bestFit;
 	if (size > 0) {
@@ -44,6 +52,34 @@ vector<Point> initFitLine(const vector<vector<Point>>& points, int index) {
 
 	return bestFit;
 }
+int getShiftAmount(int x) {
+	// We want to shift the x into the middle to figure out the angle that would be made between these two lines
+	int shiftTarget = 320;
+	return shiftTarget - x;
+}
+float angleBetween(const Point &v1, const Point &v2) {
+	float len1 = sqrt(v1.x * v1.x + v1.y * v1.y);
+	float len2 = sqrt(v2.x * v2.x + v2.y * v2.y);
+
+	float dot = v1.x * v2.x + v1.y * v2.y;
+
+	float a = dot / (len1 * len2);
+
+	if (a >= 1.0)
+		return 0.0;
+	else if (a <= -1.0)
+		return CV_PI; //PI
+	else
+		return acos(a); // 0..PI
+}
+
+double getLength(Point p1, Point p2) {
+	int xVal = pow(p2.x - p1.x, 2);
+	int yVal = pow(p2.y - p1.y, 2);
+	int distance = sqrt(xVal + yVal);
+	return distance;
+}
+
 int main(int argc, char *argv[]) {
 	Mat input = imread(argv[1]);
 	if (input.empty()) {
@@ -72,6 +108,7 @@ int main(int argc, char *argv[]) {
 	//waitKey();
 
 	VideoCapture cap("video7.mp4");
+	//VideoCapture cap(0);
 	if (!cap.isOpened()) {
 		return -1;
 	}
@@ -82,31 +119,40 @@ int main(int argc, char *argv[]) {
 	int numAvgLines = 0;
 	Point topAvg(0, 0);
 	Point botAvg(0, 0);
+	Point topTemp(0, 0);
+	Point botTemp(0, 0);
+
+	// For triangle calibration to help eliminate issues with jumping line
+	// @@ int prevY: Y value for previous frame from triangle
+	// @@ int currentY: current Y value for the frame
+	int prevY = -1;
+	int currentY = -1;
+	int xVals[] = {0, 0, 0, 0};
+
+
+	
 	while (cap.isOpened()) {
 		frame;
 		cap >> frame;
 		if (frame.empty()) {
 			break;
 		}
+
 		processCount++;
 		if (processCount > processFrames) {
 			// Masks image for yellow color
 			Mat yellowOnly = yellowFilter(frame);
-			// Draw a line across middle of frame for ROI
-			line(frame, Point(0, 220),
-				Point(640, 220), Scalar(0, 0, 0), 3, 8);
-			// Draw line segments for left, right, and middle
-			line(frame, Point(213, 220),
-				Point(213, 480), Scalar(0, 0, 0), 3, 8);
-			line(frame, Point(426, 220),
-				Point(426, 480), Scalar(0, 0, 0), 3, 8);
+
+			// Draws a line straight down middle of screen as a reference line
+			line(frame, Point(320, 0),
+				Point(320, 480), Scalar(0, 0, 0), 3, 8);
 
 			//////////////////////////////////////////////
 			int thresh = 50;
 			Mat input_blur;
 			GaussianBlur(input, input_blur, Size(25, 25), 0, 0);
 			Mat canny_output;
-			vector<vector<Point> > contours;
+			vector<vector<Point>> contours;
 			vector<Vec4i> hierarchy;
 
 
@@ -115,6 +161,7 @@ int main(int argc, char *argv[]) {
 			/// Find contours
 			findContours(yellowOnly, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
+			imshow("canny_output", canny_output);
 			//rectangle(frame, Point(130, 350),
 			//	Point(500, 400), Scalar(0, 255, 0), 1, 8);
 
@@ -193,7 +240,7 @@ int main(int argc, char *argv[]) {
 				//line(drawing, Point(bestFitLine[0], bestFitLine[1]), Point(bestFitLine[2], bestFitLine[3]), color, 1, 8);
 
 
-				imshow("Contours", drawing);
+				//imshow("Contours", drawing);
 			}
 			//waitKey(0);
 			//}
@@ -206,7 +253,8 @@ int main(int argc, char *argv[]) {
 			// Create a vectorwhich contains 4 integers in each element (coordinates of the line)
 			vector<Vec4i> lines;
 
-
+			//float testAngle = atan2(-1 - -1, 2 - 10);
+			//cout << "testAngle = " << testAngle << endl;
 			// Set limits on line detection
 			//Good value here
 			//double minLineLength = 80;
@@ -271,33 +319,84 @@ int main(int argc, char *argv[]) {
 				lowestPoint.x /= numLines;
 				line(frame, highestPoint,
 					lowestPoint, Scalar(0, 0, 255), 3, 8);
-
-
+				topAvg.x += highestPoint.x;
+				topAvg.y += highestPoint.y;
+				botAvg.x += lowestPoint.x;
+				botAvg.y += lowestPoint.y;
 				// Every 5 frames add to the average line
-				if ((processCount > processFrames) && (processCount % 5 == 0)) {
+				if (processCount % 5 == 0) {
 					numAvgLines++;
-					cout << "highestPoint = (" << highestPoint.x << ", " << highestPoint.y << ")" << endl;
-					cout << "topAvg = (" << topAvg.x << ", " << topAvg.y << ")" << endl;
-					cout << "numAvgLines = " << numAvgLines << endl;
-					circle(frame, topAvg, 5, (255, 255, 255), -1);
-					topAvg.x = highestPoint.x;
-					topAvg.y = highestPoint.y;
-					botAvg.x = lowestPoint.x;
-					botAvg.y = lowestPoint.y;
+					topAvg.x /= 5;
+					topAvg.y /= 5;
+					botAvg.x /= 5;
+					botAvg.y /= 5;
+					topTemp = topAvg;
+					botTemp = botAvg;
+					topAvg.x = 0;
+					topAvg.y = 0;
+					botAvg.x = 0;
+					botAvg.y = 0;
 
+				}
+
+
+				circle(frame, topAvg, 5, (255, 255, 255), -1);
+				
+				if ((botTemp.x != 0) && (botTemp.y != 0)) {
+
+					line(frame, topTemp,
+						botTemp, Scalar(0, 255, 0), 3, 8);
+					int midX = (botTemp.x + topTemp.x) / 2;
+					int midY = (botTemp.y + topTemp.y) / 2;
+					circle(frame, Point(midX, midY), 5, (0, 0, 0), -1);
+					int shiftAmount = getShiftAmount(midX);
+					int mid = (topTemp.x + shiftAmount + botTemp.x + shiftAmount) / 2;
+
+					Point topTrans(topTemp.x + shiftAmount, topTemp.y);
+					Point botTrans(botTemp.x + shiftAmount, botTemp.y);
+
+					// Each line here is drawn and forms a triangle which we use to calculate the angel we need
+					// to turn to stay on the line
+
+					// Draws the shifted line onto the middle of the image
+					line(frame, botTrans,
+						topTrans, Scalar(255, 255, 0), 3, 8);
+					
+					// Draws the line from the middle point of the average line found
+					// and draws from its middle point to the center of the image
+					line(frame, Point(midX, midY),
+						Point(320, midY), Scalar(255, 255, 0), 3, 8);
+					
+					int diff = 320 - midX;
+					cout << "diff = " << diff << endl;
+
+					// Draws line from top of transposed line to the middle of image
+					line(frame, topTrans,
+						Point(320,topTrans.y), Scalar(255, 255, 0), 3, 8);
+
+
+					//float angle2 = atan2(p1.y - p2.y, p1.x - p2.x);
+					float angle2 = atan2(midY - topTemp.y, 320 - topTemp.x);
+					cout << "angle2 = " << angle2 * 180 / CV_PI<< endl;
+					float angle = angleBetween(Point(320, midY), Point(topTemp.x + shiftAmount, topTemp.y));
+					//circle(frame, Point(320, topAvg.y), 10, (127, 127, 127), -1);
+					circle(frame, Point(topTemp.x + shiftAmount, topTemp.y), 5, (255, 255, 127), -1);
+					cout << "angle = " << angle  << endl;
+					double opposite = getLength(topTrans, Point(320, topTrans.y));
+					cout << "opposite = " << opposite << endl;
+					double hypotenuse = getLength(topTrans, Point(320, midY));
+					circle(frame, Point(320, midY), 5, (255, 255, 127), -1);
+					cout << "hypotenuse = " << hypotenuse << endl;
+					cout << "O/H = " << opposite / hypotenuse << endl;
+					double testAngle = asin(opposite / hypotenuse) * 180 / CV_PI;
+					cout << "testAngle = " << testAngle << endl;
+					waitKey(0);
 				}
 			}
 			
-			line(frame, topAvg,
-			botAvg, Scalar(0, 255, 0), 3, 8);
+
 			
 
-
-			// Draw borders for turn radius
-			//for (int i = 0; i < 10; i++) {
-			//	int interval = 64;
-			//	line(frame, Point(interval * i, 480), Point(interval * i, 0), Scalar(255, 0, 0), 1, 8);
-			//}
 			//imshow("canny_output", canny_output);
 			imshow("yellowOnly", yellowOnly);
 			imshow("frame", frame);
